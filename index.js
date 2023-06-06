0a25b1f08860f28f1e452a15e6a2ae5f6cec2451
Jeremy Corbeil <jeremycorbeil9@yahoo.com>
2023-06-06 11:33:32 -0400

    finished phase-1-array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin(array) {
+  const win = array.find(obj => obj.result === "W");
+  return win ? win.year : undefined;
+}
+const games = [
+  { year: 2019, result: "L" },
+  { year: 2020, result: "W" },
+  { year: 2021, result: "L" },
+];
+
+console.log(superbowlWin(games)); // Output: 2020
